d1a32c10cdb595228100e4836cdfaf7aec68a2a0
saividadala <sai.vidadala01@gmail.com>
2017-03-03 04:43:04 +0530

    Add files via upload

---
+++ mycode.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    $(".test").click(function() {
+        $(".dropone").append("<p>The server has received the request headers and the client should proceed to send the request body (in the case of a request for which a body needs to be sent; for example, a POST request).</p>.")
+    
+    });
+        $(".test1").click(function() {
+        $(".dropone").append("<p>The requester has asked the server to switch protocols and the server has agreed to do so</p>.")
+    });
+    
+   
+        });
+    
+    
+    
